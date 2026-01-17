1943f1ad52bbb96a9bd2348a797efbe3e1e97747
dev-komenzar <takuya.kometan@gmail.com>
2026-01-17 16:42:17 +0900

    update: アイキャッチのデザイン変更

--- src/routes/section-eyecatch.svelte
+++ src/routes/section-eyecatch.svelte
@@ -5,7 +5,20 @@
 <section id="eyecatch" class="eyecatch-section">
 	<div class="hero-container eyecatch-container">
 		<div class="eyecatch-content">
-			<h1 class="eyecatch-title">仏教、かもしれない</h1>
+			<p class="eyecatch-meta">OUR MISSION</p>
+			<h1 class="eyecatch-title">「仏教かもしれない」</h1>
+
+			<p class="eyecatch-description">
+				宗教や伝統文化としてではない、純粋な「思想・哲学」としての仏教は、<br/>
+				最もフラットで論理破綻のない、人類のOSとしての最高到達地点ではないか？<br/>
+				次の時代の人類哲学は「仏教かもしれない」
+			</p>
+			<p class="eyecatch-description">
+				長い歴史と、文化的な経緯によって「宗教」の衣を纏い<br/>
+				「使いにくくなってしまった」仏教思想を、もう一度「普遍的な人類の哲学」に戻す。<br/>
+				原題の人類社会に、使いやすいものへと橋を渡す。<br/>
+				「誰もが使える仏教思想」の提供を目指します。
+			</p>
 		</div>
 	</div>
 	
@@ -28,13 +41,33 @@
 		max-width: 800px;
 	}
 
+	.eyecatch-meta{
+		margin-bottom: 32px;
+		font-family: var(--font-sans-bold);
+		font-size: 36px;
+		line-height: 44px;
+		letter-spacing: -0.01em;
+	}
+
 	.eyecatch-title {
 		padding: 0 var(--space-4);
-		margin: 0;
-		font-family: var(--font-mincho);
+		margin: 0 0 33px;
+		font-family: var(--font-gothic-bold);
+		font-size: 25px;
 		line-height: 1.4;
 		color: var(--color-white);
 		text-align: center;
-		letter-spacing: 0.05em;
+		letter-spacing: 0.06em;
+	}
+
+	.eyecatch-description {
+		font-family: var(--font-gothic);
+		font-size: 14px;
+		line-height: 24px;
+		letter-spacing: 0.06em;
+	}
+
+	.eyecatch-description + .eyecatch-description {
+		margin: 30px 0 0;
 	}
 </style>
